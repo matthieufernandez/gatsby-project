2870d7ee4d132c7072f6c61dc906f64f7c19b2f2
Matthieu Fernandez <clockwork_mango@hotmail.com>
2022-06-18 13:55:03 -0400

    adding pages and navbar

---
+++ gatsby-starter-hello-world/src/pages/projects/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function index() {
+  return (
+    <div>
+      <h1>This is a page for our projects.</h1>
+      <p>Here are some projects:</p>
+    </div>
+  )
+}
