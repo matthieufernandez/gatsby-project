b13d6f6315468ddfe4ee1957e52c9e5bb9d8a771
Matthieu Fernandez <clockwork_mango@hotmail.com>
2022-06-19 10:40:38 -0400

    adding projects data

--- gatsby-starter-hello-world/gatsby-config.js
+++ gatsby-starter-hello-world/gatsby-config.js
@@ -7,13 +7,7 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    // You can have multiple instances of this plugin
-    // to read source nodes from different locations on your
-    // filesystem.
-    //
-    // The following sets up the Jekyll pattern of having a
-    // "pages" directory for Markdown files and a "data" directory
-    // for `.json`, `.yaml`, `.csv`.
+    "gatsby-transformer-remark",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- gatsby-starter-hello-world/src/pages/projects/index.js
+++ gatsby-starter-hello-world/src/pages/projects/index.js
@@ -1,14 +1,50 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
-import { portfolio } from "../../styles/projects.module.css"
+import { portfolio, projects } from "../../styles/projects.module.css"
 
-export default function index() {
+export default function index({ data }) {
+  const projectInfo = data.allMarkdownRemark.nodes
+
   return (
     <Layout>
       <div className={portfolio}>
-        <h2>This is a page for my projects.</h2>
+        <h2>These are the projects I've worked on.</h2>
         <h3>Feel free to browse</h3>
+
+        <div className={projects}>
+          {projectInfo.map(project => {
+            return (
+              <Link
+                key={project.id}
+                to={`/projects/${project.frontmatter.slug}`}
+              >
+                <div>
+                  <h4>{project.frontmatter.title}</h4>
+                  <p>{project.frontmatter.stack}</p>
+                </div>
+              </Link>
+            )
+          })}
+        </div>
       </div>
     </Layout>
   )
 }
+
+// export page query
+
+export const query = graphql`
+  query projectInfo {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+        }
+        id
+      }
+    }
+  }
+`
